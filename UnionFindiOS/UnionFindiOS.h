d54c82387a2508fd273176f0f9c65db3aa71aa6b
Igor Ranieri Elland <igor@elland.me>
2016-11-24 14:31:04 +0100

    Added shared framework target for Carthage support.

---
+++ UnionFindiOS/UnionFindiOS.h
@@ -0,0 +1,20 @@
+//
+//  UnionFindiOS.h
+//  UnionFindiOS
+//
+//  Created by Igor Ranieri on 24/11/2016.
+//  Copyright © 2016 Strilanc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnionFindiOS.
+FOUNDATION_EXPORT double UnionFindiOSVersionNumber;
+
+//! Project version string for UnionFindiOS.
+FOUNDATION_EXPORT const unsigned char UnionFindiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnionFindiOS/PublicHeader.h>
+
+#import <UnionFindiOS/UnionFind.h>
+#import <UnionFindiOS/UFDisjointSetNode.h>
